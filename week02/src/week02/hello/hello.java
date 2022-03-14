e08d4dbc50e5c1351f11879977d7582cfb233737
jykhpw <jykhpw994@gmail.com>
2022-03-14 11:10:22 +0900

    [JAVA] hello.java 생성 hello world를 출력하는 코드 작성

---
+++ week02/src/week02/hello/hello.java
@@ -0,0 +1,11 @@
+package week02.hello;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+		System.out.println("I'm a new Java Programmer");
+	}
+
+}
