776c99a18fe9a30d730d081716619e8d7f7cebb0
jykhpw <jykhpw994@gmail.com>
2022-03-23 00:19:17 +0900

    [Week02] 실습 2, 변수와 자료형
    반지름이 3.0인 원의 면적을 계산하는 프로그램을 작성

---
+++ AreaTest/src/AreaTest.java
@@ -0,0 +1,13 @@
+
+public class AreaTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double PI = 3.141592;
+		double radius = 3.0;
+		double area = radius * radius * PI;
+		
+		System.out.println("반지름이 " + radius + "인 원의 면적은 " + area + "입니다.");
+	}
+
+}
