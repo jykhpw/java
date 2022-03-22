4cc1c8189f03a39841c8c588d44b1c25df28818a
jykhpw <jykhpw994@gmail.com>
2022-03-23 00:26:07 +0900

    [Week 02] 실습 4. 윤년 검사 프로그램
    2024년이 윤년인지 확인하는 프로그램을 작성함 (4로 나누어 떨어지는 해 = 윤년)
    윤년인 경우 결과를 true, 아니면 false를 출력함(boolean)

---
+++ LeapYear/src/LeapYear.java
@@ -0,0 +1,12 @@
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int year = 2024;
+		boolean leapYear = (year % 4 == 0);
+		
+		System.out.println("Result: " + leapYear);
+	}
+
+}
