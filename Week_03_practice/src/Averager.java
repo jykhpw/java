ef20e2712763f3366b7ba207ec7309afaed27235
jykhpw <jykhpw994@gmail.com>
2022-03-23 12:40:43 +0900

    [Week 03] break문, continue문 실습
    Averager.java (break문 실습)
    ContinueTest.java (continue문 실습)

---
+++ Week_03_practice/src/Averager.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Averager {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int total = 0;
+		int count = 0;
+		Scanner scan = new Scanner(System.in);
+		while(true) {
+			System.out.print("점수를 입력하세요: ");
+			int grade = scan.nextInt();
+			if (grade < 0)
+				break;
+			
+			total += grade;
+			count++;
+		}
+		System.out.println("평균은 " + total/count);
+	}
+
+}
