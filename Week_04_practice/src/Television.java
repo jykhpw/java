6a4631016f03b0c4e5ad6775c7f0cd5090d59f79
jykhpw <jykhpw994@gmail.com>
2022-03-24 11:25:09 +0900

    [Week 04] 객체지향 프로그래밍 개념
    television 클래스의 channel, volume, onOff 필드와 print() 메소드
    
    -> 기존의 television 클래스에 print() 메소드를 추가하고 TelevisionTest.java를 수정함

--- Week_04_practice/src/Television.java
+++ Week_04_practice/src/Television.java
@@ -3,4 +3,9 @@ public class Television {
 	int channel; // 채널번호
 	int volume; // 볼륨
 	boolean onOff; // 전원 상태
+	
+	void print() {
+		System.out.println("채널은 " + channel + "이고, 볼륨은 " +
+				volume + "입니다.");
+	}
 }
