09838c9309efaccf21092bad000bc7d4ff34f9c4
jykhpw <jykhpw994@gmail.com>
2022-04-15 16:48:21 +0900

    [Week07] 04/14 Thur
    ShapeTest 코드 수정
    : p.72 동적 바인딩

--- Week_07_practice/src/ShapeTest.java
+++ Week_07_practice/src/ShapeTest.java
@@ -28,20 +28,25 @@ class Circle extends Shape {
 }
 
 public class ShapeTest {
+	
+	private static Shape arrayOfShapes[];
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Shape s1, s2, s3, s4;
-		
-		s1 = new Shape();
-		s2 = new Rectangle();
-		s3 = new Triangle();
-		s4 = new Circle();
-		
-		s1.draw();
-		s2.draw();
-		s3.draw();
-		s4.draw();
+		init();
+		drawAll();
+	}
+	
+	public static void init() {
+		arrayOfShapes = new Shape[3];
+		arrayOfShapes[0] = new Rectangle();
+		arrayOfShapes[1] = new Triangle();
+		arrayOfShapes[2] = new Circle();
+	}
+	
+	public static void drawAll() {
+		for (int i = 0; i < arrayOfShapes.length; i++) {
+			arrayOfShapes[i].draw();
+		}
 	}
 
 }
