ebcf73664fbb77f9d773765bf77b2a6788b220f2
jykhpw <jykhpw994@gmail.com>
2022-04-28 10:19:42 +0900

    Object-Oriented Programming - 04/28 Thur.
    기초 컴포넌트 확인 예제 - 아이콘(icon.gif) 이미지 삽입
    : MyFrame 소스 코드 수정, icon.gif 이미지 파일

--- Week_09_practice/src/MyFrame.java
+++ Week_09_practice/src/MyFrame.java
@@ -1,12 +1,17 @@
 import javax.swing.*;
-import java.awt.FlowLayout;
+import java.awt.*;
 
 public class MyFrame extends JFrame{ // JFrame을 상속해서 MyFrame을 정의
 	public MyFrame() {
+		Toolkit kit = Toolkit.getDefaultToolkit();
+		Dimension screenSize = kit.getScreenSize();
 		setSize(300, 200);
+		setLocation(screenSize.width/2, screenSize.height/2);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("MyFrame");
 		
+		Image img = kit.getImage("icon.gif");
+		setIconImage(img);
 		setLayout(new FlowLayout()); // 배치 관리자 설정
 		JButton button = new JButton("버튼"); // 컴포넌트 생성 및 추가
 		this.add(button);
